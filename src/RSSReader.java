a26bd2411716c00cfb9212f0a7684a1593d520cb
Parsa Hosseini <parsahosseini2001@gmail.com>
2024-02-24 15:10:12 +0330

    Add getPageSource()

--- src/RSSReader.java
+++ src/RSSReader.java
@@ -7,24 +7,34 @@ public class RSSReader
 
     public static void main(String[] args) throws Exception
     {
-        BufferedReader in = getIn("https://www.rkneusel.com/");
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
+        System.out.println(getPageSource("https://www.rkneusel.com/"));
     }
 
-    public static BufferedReader getIn(String urlString) throws Exception
+    public static String getPageSource(String urlString) throws Exception
     {
         try
         {
             URI uri = new URI(urlString);
             URL url = uri.toURL();
-            return new BufferedReader(new InputStreamReader(url.openStream()));
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
+            return toString(urlConnection.getInputStream());
         }
         catch (MalformedURLException e)
         {
             throw new RuntimeException(e);
+        }
+    }
+    private static String toString(InputStream inputStream) throws IOException
+    {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8")))
+        {
+            String inputLine;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((inputLine = bufferedReader.readLine()) != null)
+                stringBuilder.append(inputLine);
+
+            return stringBuilder.toString();
         }
     }
 }
