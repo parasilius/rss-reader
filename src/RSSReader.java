8b15aeb1898c99b42229f4a95499a57452678ca7
Parsa Hosseini <parsahosseini2001@gmail.com>
2024-02-27 19:49:42 +0330

    Add single url functionality

--- src/RSSReader.java
+++ src/RSSReader.java
@@ -1,21 +1,63 @@
 import org.jsoup.*;
-import org.jsoup.nodes.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.net.*;
 import java.io.*;
 
 public class RSSReader
 {
-    private String[] urls;
-
     public static void main(String[] args) throws Exception
     {
-        String html = fetchPageSource("https://magazine.sebastianraschka.com/");
-        System.out.println(extractRssUrl(html));
+        String url = "https://www.omgubuntu.co.uk/";
+        String html = fetchPageSource(url);
+        System.out.println(extractPageTitle(html));
+        retrieveRssContent(extractRssUrl(html));
+    }
+
+    public static String extractPageTitle(String html) throws NullPointerException
+    {
+        try
+        {
+            org.jsoup.nodes.Document doc = Jsoup.parse(html);
+            return doc.select("title").first().text();
+        }
+        catch (Exception e)
+        {
+            return "Error: no title tag found in page source!";
+        }
+    }
+
+    public static void retrieveRssContent(String rssUrl) throws Exception
+    {
+        String rssXml = fetchPageSource(rssUrl);
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        StringBuilder xmlStringBuilder = new StringBuilder();
+        xmlStringBuilder.append(rssXml);
+        ByteArrayInputStream input = new ByteArrayInputStream(
+                xmlStringBuilder.toString().getBytes("UTF-8"));
+        org.w3c.dom.Document doc = documentBuilder.parse(input);
+        NodeList itemNodes = doc.getElementsByTagName("item");
+
+        for (int i = 0; i < itemNodes.getLength(); ++i)
+        {
+            Node itemNode = itemNodes.item(i);
+            if (itemNode.getNodeType() == Node.ELEMENT_NODE)
+            {
+                Element element = (Element) itemNode;
+                System.out.println("Title: " + element.getElementsByTagName("title").item(0).getTextContent());
+                System.out.println("Link: " + element.getElementsByTagName("link").item(0).getTextContent());
+                System.out.println("Description: " + element.getElementsByTagName("description").item(0).getTextContent());
+            }
+        }
     }
 
     public static String extractRssUrl(String html)
     {
-        Document doc = Jsoup.parse(html);
+        org.jsoup.nodes.Document doc = Jsoup.parse(html);
         return doc.select("[type='application/rss+xml']").attr("href");
     }
 
